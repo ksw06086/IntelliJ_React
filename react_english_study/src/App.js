bd73e580ce47229e1d0cc6ae770d76cb5f69e250
ksw06086 <k-sunwo@naver.com>
2023-04-22 06:41:25 +0900

    react_english_study: css 작성방법

--- react_english_study/src/App.js
+++ react_english_study/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Hello from './component/Hello';
 import Welcome from "./component/Welcome";
+import styles from "./App.module.css"
 
 // 이것이 하나의 컴포넌트, 컴포넌트는 항상 대문자로 시작함
 // 함수와 리턴하는 것은 JSX(javascript XML)
@@ -8,9 +9,7 @@ function App() {
   return (
     <div className="App">
       <Hello/>
-        <Hello/>
-        <Hello/>
-      <Welcome/>
+      <div className={styles.box}>App</div>
     </div>
   );
 }
--- react_english_study/src/component/Hello.js
+++ react_english_study/src/component/Hello.js
@@ -7,14 +7,22 @@ export default Hello;*/
 
 // ex2)
 import World from "./World";
+import styles from "./Hello.module.css"
 
 // JSX는 하나의 태그만 들어갈 수 있다.
 export default function Hello(){
     return (
         <>
-            <h1>Hello</h1>
-            <World/>
-            <World/>
+            <h1 style={
+                {
+                    color: '#f00',
+                    borderRight: '12px solid #000',
+                    marginBottom: '30px',
+                    opacity: 1,
+                }
+            }
+            >Hello</h1>
+            <div className={styles.box}>Hello</div>
         </>
     );
 }
