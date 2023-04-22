b597bc51460cfd1214918356fd5db6bae06d1ff0
ksw06086 <k-sunwo@naver.com>
2023-04-22 20:13:01 +0900

    english_study: 라우터 구현, react-router-dom

--- react_english_study/src/App.js
+++ react_english_study/src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 // 이것이 하나의 컴포넌트, 컴포넌트는 항상 대문자로 시작함
 // 함수와 리턴하는 것은 JSX(javascript XML)
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <DayList/>
-        <Day/>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path="/" element={<DayList/>}/>
+                <Route path="/day/:day" element={<Day/>}/>
+                <Route path={"*"} element={<EmptyPage/>}/>
+            </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- react_english_study/src/component/Day.js
+++ react_english_study/src/component/Day.js
@@ -1,22 +1,27 @@
 import dummy from '../db/data.json'
+import {useParams} from "react-router-dom";
 
 export default function Day() {
     // dummy.words
     // 단어의 날짜가 1인 요소들만 출력
+    const { day } = useParams(); // 얘가 문자임
     const wordList = dummy.words.filter(word => (
-        word.day === 1
+        word.day === Number(day)
     ))
 
     return (
-        <table>
-            <tbody>
-            {wordList.map(word => (
-                <tr>
-                    <td>{word.eng}</td>
-                    <td>{word.kor}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+        <>
+            <h2>Day {day}</h2>
+            <table>
+                <tbody>
+                {wordList.map(word => (
+                    <tr>
+                        <td>{word.eng}</td>
+                        <td>{word.kor}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </>
     )
 }
