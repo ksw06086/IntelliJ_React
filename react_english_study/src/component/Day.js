b14f250935c10c3d9a01eb349bd50fe5c477b889
ksw06086 <k-sunwo@naver.com>
2023-04-23 23:17:49 +0900

    english_study: json-server, Rest API 개념

--- react_english_study/src/component/Day.js
+++ react_english_study/src/component/Day.js
@@ -1,5 +1,6 @@
 import dummy from '../db/data.json'
 import {useParams} from "react-router-dom";
+import Word from "./Word";
 
 export default function Day() {
     // dummy.words
@@ -15,10 +16,7 @@ export default function Day() {
             <table>
                 <tbody>
                 {wordList.map(word => (
-                    <tr>
-                        <td>{word.eng}</td>
-                        <td>{word.kor}</td>
-                    </tr>
+                    <Word word={word} key={word.id} />
                 ))}
                 </tbody>
             </table>
