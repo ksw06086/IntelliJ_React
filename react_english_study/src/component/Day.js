82e143af1340f20232c552e837a74426afe9136f
ksw06086 <k-sunwo@naver.com>
2023-04-25 09:53:53 +0900

    english_study: useEffect, fetch()

--- react_english_study/src/component/Day.js
+++ react_english_study/src/component/Day.js
@@ -1,21 +1,32 @@
 import dummy from '../db/data.json'
 import {useParams} from "react-router-dom";
 import Word from "./Word";
+import {useEffect, useState} from "react";
 
 export default function Day() {
     // dummy.words
     // 단어의 날짜가 1인 요소들만 출력
     const { day } = useParams(); // 얘가 문자임
-    const wordList = dummy.words.filter(word => (
-        word.day === Number(day)
-    ))
+    const [words, setWords] = useState([])
+
+    // useEffect안에 변수를 사용했는데 그럼 변할 수 있는 값인데 한번만 실행되다보니
+    // 변경되어도 반영이 안된다고 하여 경고가 나옴 그래서 아래에 day를 배열에 넣어줘서
+    // 변경이 되면 반영될 수 있도록 해주어야 함
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+            .then(res => {
+                return res.json();
+            }).then(data => {
+                setWords(data)
+            })
+    }, [day])
 
     return (
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                {wordList.map(word => (
+                {words.map(word => (
                     <Word word={word} key={word.id} />
                 ))}
                 </tbody>
--- react_english_study/src/component/DayList.js
+++ react_english_study/src/component/DayList.js
@@ -1,14 +1,31 @@
-import dummy from '../db/data.json'
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function DayList(){
+    const [days, setDays] = useState([]);
+
+    // 의존성 배열, count가 바뀔 때만 실행됨
+    // fetch(API경로) : API 비동기 통신을 위해 사용, Promiss가 반환됨
+    // res는 http 응답이고 실제 json은 아님 그래서 json을 사용하면 json으로 변환되고 Promiss를 반환해줌
+    useEffect(() => {
+        fetch("http://localhost:3001/days")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setDays(data);
+            });
+    }, [])
+
     return (
-        <ul className="list_day">
-            {dummy.days.map(day => (
-                <li key={day.id}>
-                    <Link to ={`/day/${day.day}`}>Day {day.day}</Link>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul className="list_day">
+                {days.map(day => (
+                    <li key={day.id}>
+                        <Link to ={`/day/${day.day}`}>Day {day.day}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
